2fcca034f7c31e8f556b15388f81213995c51faf
javierpb275 <javierpb275@gmail.com>
2021-09-16 18:14:55 +0200

    added layout and navigation components

---
+++ components/layout/main-navigation.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+const MainNavigation = () => {
+  return (
+    <header>
+      <Link href="/">
+        <a>Logo</a>
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/posts">Posts</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
