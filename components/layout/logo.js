54c26aa367346153422989ea3a9e1dd95c21d72c
javierpb275 <javierpb275@gmail.com>
2021-09-16 18:26:09 +0200

    added styles and logo component

---
+++ components/layout/logo.js
@@ -0,0 +1,11 @@
+import classes from './logo.module.css';
+
+const Logo = () => {
+    return(
+        <div className={classes.logo}>
+            Javier's Next Blog
+        </div>
+    );
+}
+
+export default Logo;
--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,10 +1,14 @@
 import Link from "next/link";
+import classes from "./main-navigation.module.css";
+import Logo from "./logo";
 
 const MainNavigation = () => {
   return (
-    <header>
+    <header className={classes.header}>
       <Link href="/">
-        <a>Logo</a>
+        <a>
+          <Logo />
+        </a>
       </Link>
       <nav>
         <ul>
