32ddc6528063712b79260b2660a3328fa7fc1625
javierpb275 <javierpb275@gmail.com>
2021-09-18 14:06:41 +0200

    Rendering Dummy Post Data

--- components/home-page/featured-posts.js
+++ components/home-page/featured-posts.js
@@ -1,11 +1,13 @@
-import classes from './featured-posts.module.css';
+import PostsGrid from "../posts/posts-grid";
+import classes from "./featured-posts.module.css";
 
-const FeaturedPosts = () => {
-    return(
-        <section className={classes.latest}>
-            <h2>Featured Posts</h2>
-        </section>
-    );
-}
+const FeaturedPosts = (props) => {
+  return (
+    <section className={classes.latest}>
+      <h2>Featured Posts</h2>
+      <PostsGrid posts={props.posts} />
+    </section>
+  );
+};
 
 export default FeaturedPosts;
--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -1,4 +1,4 @@
-import classes from "./posts-item.module.css";
+import classes from "./post-item.module.css";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -10,12 +10,13 @@ const PostItem = (props) => {
     year: "numeric",
   });
   const imagePath = `/images/posts/${slug}/${image}`;
+  const linkPath = `/posts/${slug}`
   return (
     <li className={classes.post}>
-      <Link>
+      <Link href={linkPath}>
         <a>
           <div className={classes.image}>
-            <Image src={imagePath} alt={title} width={300} height={200} />
+            <Image src={imagePath} alt={title} width={300} height={200} layout="responsive"/>
           </div>
           <div className={classes.content}>
             <h3>{title}</h3>
