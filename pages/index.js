1ca1edb54cbcc3a68d677cb1a181d5e5da85dac8
javierpb275 <javierpb275@gmail.com>
2021-09-18 17:19:16 +0200

    added the all posts page

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,15 @@ const DUMMY_POSTS = [
     excerpt:
       "Next.js is the React Framework for Production. It makes building fullstack React apps and sites very easy.",
     date: "2021-09-18",
-  }
+  },
+  {
+    slug: "best-framework-ever-4",
+    title: "Best Framework Ever",
+    image: "nextjs.png",
+    excerpt:
+      "Next.js is the React Framework for Production. It makes building fullstack React apps and sites very easy.",
+    date: "2021-09-18",
+  },
 ];
 
 const HomePage = () => {
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,5 +1,42 @@
+import AllPosts from "../../components/posts/all-posts";
+
+const DUMMY_POSTS = [
+  {
+    slug: "best-framework-ever",
+    title: "Best Framework Ever",
+    image: "nextjs.png",
+    excerpt:
+      "Next.js is the React Framework for Production. It makes building fullstack React apps and sites very easy.",
+    date: "2021-09-18",
+  },
+  {
+    slug: "best-framework-ever-2",
+    title: "Best Framework Ever",
+    image: "nextjs.png",
+    excerpt:
+      "Next.js is the React Framework for Production. It makes building fullstack React apps and sites very easy.",
+    date: "2021-09-18",
+  },
+  {
+    slug: "best-framework-ever-3",
+    title: "Best Framework Ever",
+    image: "nextjs.png",
+    excerpt:
+      "Next.js is the React Framework for Production. It makes building fullstack React apps and sites very easy.",
+    date: "2021-09-18",
+  },
+  {
+    slug: "best-framework-ever-4",
+    title: "Best Framework Ever",
+    image: "nextjs.png",
+    excerpt:
+      "Next.js is the React Framework for Production. It makes building fullstack React apps and sites very easy.",
+    date: "2021-09-18",
+  },
+];
+
 const AllPostsPage = () => {
-  return <div>All Posts Page</div>;
+  return <AllPosts posts={DUMMY_POSTS} />;
 };
 
 export default AllPostsPage;
