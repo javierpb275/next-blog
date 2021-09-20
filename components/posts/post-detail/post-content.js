89ea28288c7e4067855ecca43cf438a7ba91471c
javierpb275 <javierpb275@gmail.com>
2021-09-20 19:29:01 +0200

    rendering dynamic post pages and paths

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -2,22 +2,13 @@ import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
 import ReactMarkdown from "react-markdown";
 
-const DUMMY_POST = {
-  slug: "best-framework-ever",
-  title: "Best Framework Ever",
-  image: "nextjs.png",
-  excerpt:
-    "Next.js is the React Framework for Production. It makes building fullstack React apps and sites very easy.",
-  date: "2021-09-18",
-  content: "# This is a first post",
-};
-
-const PostContent = () => {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+const PostContent = (props) => {
+  const { post } = props;
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   );
 };
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,7 +1,33 @@
 import PostContent from "../../components/posts/post-detail/post-content";
+import { getPostData, getPostsFiles } from "../../lib/posts-util";
 
-const PostDetailPage = () => {
-  return <PostContent />;
+const PostDetailPage = (props) => {
+  return <PostContent post={props.post} />;
 };
+
+export function getStaticProps(context) {
+  const { params } = context;
+  const { slug } = params;
+  const postData = getPostData(slug);
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 600,
+  };
+}
+
+export function getStaticPaths() {
+  const postFileNames = getPostsFiles();
+  const slugs = postFileNames.map((fileName) => fileName.replace(/\.md$/, ""));
+  return {
+    paths: slugs.map((slug) => ({
+      params: {
+        slug,
+      },
+    })),
+    fallback: false,
+  };
+}
 
 export default PostDetailPage;
